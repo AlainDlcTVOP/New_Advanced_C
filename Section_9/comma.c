4ef9daed9f81006fb75e218b56486e9dcb533905
AlainDlcTVOP <alain_dlc@outlook.com>
2020-07-27 10:17:35 +0200

    commaoperant

---
+++ Section_9/comma.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    char data[50];
+    int i = 0, sum, data;
+    while (i < 100)
+        sum += data[i], ++i;
+
+    int i(5, 10); // 10 is assigned to i
+
+    int j = (f1(), f2()); // f1 be called then fallowing f2
+    int z, y, x;
+    x = (y = 3, (z = ++y + y) + 5); // z = 13
+
+    for (i = 0; j = 100; i != 10; i++, j -= 10)
+
+        printf("king"),
+            printf("queen"),
+            printf("prince");
+
+    printf("%d", x);
+
+    int houseprice = (426, 900); // the right hand will be assigned
+    printf("houseprice %d", houseprice);
+    return 0;
+}
